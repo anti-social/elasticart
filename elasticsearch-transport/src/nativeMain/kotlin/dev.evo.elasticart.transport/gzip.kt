43e5b7f6c7a2317170efa5eabf99a072ec95d07d
Alexander Koval <kovalidis@gmail.com>
2021-02-04 21:18:47 +0200

    Fixed linux native assemble

--- elasticsearch-transport/src/nativeMain/kotlin/dev.evo.elasticart.transport/gzip.kt
+++ elasticsearch-transport/src/nativeMain/kotlin/dev.evo.elasticart.transport/gzip.kt
@@ -1,7 +1,7 @@
 package dev.evo.elasticart.transport
 
 actual class GzipEncoder : RequestEncoder {
-    override fun append(part: CharSequence) {
+    override fun append(part: CharSequence?): Appendable {
         TODO("not implemented")
     }
 
